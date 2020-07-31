9de43510af600f8538a7eeed94ae15273ac8a70d
RuslanKomarov <ruslasy@yandex.ru>
2020-07-31 11:02:30 +0300

    adding public directory

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         //chunkFilename: '[name]'.bundle.js,
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
     },
   
     module:{
