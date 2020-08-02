86d7eccac2c94f3a4ce39dfe9c821bc09ddb3479
Arkgangelocityvec <6estakov43@gmail.com>
2020-08-03 00:08:40 +0300

    style to vue

--- src/js/views/components/index.vue
+++ src/js/views/components/index.vue
@@ -2,3 +2,6 @@
     include ../../../pug/modules/index/main.pug
 </template>
 
+<style scoped lang="scss">
+   // @import "../../../scss/style.scss"
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,13 @@ module.exports = {
 
             {
                 test: /\.vue$/,
-                loader: 'vue-loader'
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                      scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+                      sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax', // <style lang="sass">
+                    }
+                }
             },
 
             {
@@ -138,5 +144,6 @@ module.exports = {
             }
 
         ]
-    }
+    },
+
 };
